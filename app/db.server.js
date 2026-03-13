5a43af417460a2e090654869a9f90f61ec706c28
Max Wieber | KleberKompass <tools@kleberkompass.de>
2026-03-13 14:05:57 +0100

    Update db.server.js

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,1 @@
-import { PrismaClient } from "@prisma/client";
-
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
-  }
-}
-
-const prisma = global.prismaGlobal ?? new PrismaClient();
-
-export default prisma;
+export const db = {};
